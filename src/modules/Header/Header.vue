615a42e9303f7aa2edf2927bf19f47be9dd9bde9
Александр Макаров <alexmk.work@gmail.com>
2023-05-09 16:23:38 +0500

    Компоненты UI-кнопок

--- src/modules/Header/Header.vue
+++ src/modules/Header/Header.vue
@@ -1,20 +1,22 @@
 <template>
     <header class="header">
-        <div class="header__inner _container">
+        <div class="header__inner">
             <img class="header__logo" :src="mainLogo"
-                 alt="Логотип verbify" >
+                 alt="Логотип verbify">
             <nav class="header__nav">
-                <a href="#">Главная</a>
-                <a href="#">О нас</a>
-                <a href="#">Контакты</a>
+                <a class="header__link" href="#">Главная</a>
+                <a class="header__link" href="#">О нас</a>
+                <a class="header__link" href="#">Контакты</a>
             </nav>
-            <button class="header__button header__button_sign-in">Регистрация</button>
-            <button class="header__button header__button_login">Войти</button>
+                <VButtonBright text="Регистрация" style="margin-left: auto;"></VButtonBright>
+                <VButtonAccent text="Войти"></VButtonAccent>
         </div>
     </header>
 </template>
 
 <script setup>
+    import VButtonBright from '@components/UI/VButtonBright.vue'
+    import VButtonAccent from '@components/UI/VButtonAccent.vue';
     import mainLogo from '@images/main-logo.svg';
 </script>
 
@@ -22,14 +24,11 @@
     .header {
         font-size: 16px;
         font-weight: 400;
-        margin-top: 8px;
 
         &__inner {
             display: flex;
-            flex-wrap: wrap;
-            gap: 12px;
             align-items: center;
-            min-width: 690px;
+            gap: 12px;
             padding: 12px;
 
             border-radius: 8px;
@@ -44,31 +43,16 @@
 
         &__nav {
             display: flex;
-            margin-left: 90px;
             gap: 25px;
+            margin-left: min(6.5%, 90px);
         }
 
-        &__button {
-            font-size: 14px;
-            font-weight: 500;
-            padding: 8px 28px;
+        &__link {
+            min-width: max-content;
 
-            border: none;
-            box-shadow: var(--card-shadow);
-            border-radius: 3px;
-            cursor: pointer;
-        }
-
-        &__button_sign-in {
-            margin-left: auto;
-            color: var(--alternate-button-text-color);
-            background-color: var(--alternate-button);
-            border: 1px solid #DBE2EA;
-        }
-
-        &__button_login {
-            color: var(--main-button-text-color);
-            background-color: var(--main-button);
+            &:hover {
+                color: red;
+            }
         }
     }
 </style>
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,6 +3,6 @@ import '@styles/general.scss';
 import { createApp } from 'vue';
 import MainPage from '@pages/index/MainPage.vue';
 
-let appNode = document.querySelector('.app');
+let appNode = document.querySelector('.app-wrapper');
 const app = createApp(MainPage);
 app.mount(appNode);
