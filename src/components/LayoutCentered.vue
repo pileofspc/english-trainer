2fda6355d2a33074d0ed5a02063502c920395621
Александр Макаров <alexmk.work@gmail.com>
2023-05-18 07:14:46 +0500

    добавлен LayoutCentered и использован на странице

---
+++ src/components/LayoutCentered.vue
@@ -0,0 +1,28 @@
+<template>
+    <Header></Header>
+    <main class="layout__main">
+        <div class="_container">
+            <slot></slot>
+        </div>
+    </main>
+    <Footer class="layout__footer"></Footer>
+</template>
+
+<script setup>
+    import Header from '@modules/Header/Header.vue';
+    import Footer from '@modules/Footer/Footer.vue';
+</script>
+
+<style lang="scss" scoped>
+
+    .layout {
+        &__main {
+            margin-top: 24px;
+            flex-grow: 1;
+        }
+
+        &__footer {
+            margin-top: 24px;
+        }
+    }
+</style>
