39dafa5e8e5c61864926e4ef6aaa41133193a7df
Александр Макаров <alexmk.work@gmail.com>
2023-06-14 19:18:07 +0500

    фиксы после изменений конфигов, удалены лишние файлы

--- src/stores/storeModal.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-
-export const useModalStore = defineStore("modal", () => {
-    const show = ref(false);
-
-    return { show };
-});
